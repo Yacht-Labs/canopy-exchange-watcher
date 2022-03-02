28772759cb8fb7d61db5fee41e29889bf350acf2
Henry Minden <henryminden@gmail.com>
2022-03-02 14:48:46 -0600

    do stuff

--- main.ts
+++ main.ts
@@ -15,7 +15,7 @@ var web3Aurora = new Web3('wss://testnet.aurora.dev');
 
 const DEV_ACCOUNT_PRIVATE_KEY = "81c7e751ce18f0e39f8881e1d4071ff851d6825988742a0569941049d7a1df38";
 const {abi: canopyAbi} = canopyVault
-const vaultContract = new web3Polygon.eth.Contract(canopyAbi as AbiItem[], '0x0e8DEF110b1fC3F649ebCd21871E8bfb2f14244D');
+const vaultContract = new web3Polygon.eth.Contract(canopyVault.abi as AbiItem[], '0x0e8DEF110b1fC3F649ebCd21871E8bfb2f14244D');
 const mintContract = new web3Aurora.eth.Contract(auroraMint.abi as AbiItem[], '0x22fF3411a761E1B795BE66902DEC86B7AA405444');
 const auroraAccount = web3Aurora.eth.accounts.privateKeyToAccount(DEV_ACCOUNT_PRIVATE_KEY);
 
@@ -32,26 +32,28 @@ let mintOptions = {
   fromBlock: 0
 };
 //TODO: Fix nonce logic
-let nonce = 12;
-function processDeposit(event) {
+
+async function processDeposit(event) {
+  let nonce = await web3Aurora.eth.getTransactionCount(auroraAccount.address);
   let to = event.returnValues.from
   let amount = event.returnValues.amount
-  let encodedABI = mintContract.methods.mint(to, amount).encodeABI()
+  let encodedABI = await mintContract.methods.mint(to, amount).encodeABI()
   console.log("-")
-  auroraAccount.signTransaction({
+  console.log(nonce);
+  let signedTx = await auroraAccount.signTransaction({
       nonce: nonce,
       to: '0x22fF3411a761E1B795BE66902DEC86B7AA405444',
       data: encodedABI,
       gas: 2000000,
       chainId: 1313161555
-  }).then(
-    (signedTx) => {web3Aurora.eth.sendSignedTransaction(signedTx.rawTransaction).then(console.log)}
-  )
-  nonce += 1;
+  })
+  
+  let send = await web3Aurora.eth.sendSignedTransaction(signedTx.rawTransaction)
+  console.log(send)
 }
 
 vaultContract.events.Deposit(vaultOptions)
-    .on('data', event => processDeposit(event))
+    .on('data', async (event) => await processDeposit(event))
     .on('changed', changed => console.log(changed))
     .on('error', err => {throw err})
     .on('connected', str => console.log(str))
